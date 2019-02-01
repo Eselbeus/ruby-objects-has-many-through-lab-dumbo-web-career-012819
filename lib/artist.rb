0eccb2af0ff7473b980c83d243aa31f9e53517b2
eselbeus <eselbeus@gmail.com>
2019-02-01 20:34:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,22 @@
 class Artist 
   
+  attr_reader :name
+  
   @@all = []
   
-  
-  
+  def initialize(name)
+    @name = name
+    
+    @@all << self
+  end
   
   def self.all 
     @@all
+  end
+  
+  def new_song(name, genre)
+    
+    
   end
   
 end
